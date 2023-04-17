fc8a40d07aa04e148eb66c33e1445edbdcf80633
Victor Borzaquel <victorborzaquel@outlook.com>
2023-04-17 14:07:44 -0300

    Complete

--- src/test/java/com/tests/santanderseleniumcalculadora/CalculatorPage.java
+++ src/test/java/com/tests/santanderseleniumcalculadora/CalculatorPage.java
@@ -1,2 +1,96 @@
-package com.tests.santanderseleniumcalculadora;public class CalculatorPage {
+package com.tests.santanderseleniumcalculadora;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CalculatorPage  {
+    private final WebDriver driver;
+
+    protected record Input(WebElement element) {
+        public String value() {
+            return element.getAttribute("value");
+        }
+    }
+
+    public CalculatorPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public Input display() {
+        WebElement element = driver.findElement(By.xpath("//*[@id='display']"));
+        return new Input(element);
+    }
+
+    public WebElement title() {
+        return driver.findElement(By.tagName("h1"));
+    }
+
+    public WebElement description() {
+        return driver.findElement(By.cssSelector("h4"));
+    }
+
+    public WebElement one() {
+        return driver.findElement(By.id("one"));
+    }
+
+    public WebElement two() {
+        return driver.findElement(By.id("two"));
+    }
+
+    public WebElement three() {
+        return driver.findElement(By.id("three"));
+    }
+
+    public WebElement four() {
+        return driver.findElement(By.id("four"));
+    }
+
+    public WebElement five() {
+        return driver.findElement(By.id("five"));
+    }
+
+    public WebElement six() {
+        return driver.findElement(By.id("six"));
+    }
+
+    public WebElement seven() {
+        return driver.findElement(By.id("seven"));
+    }
+
+    public WebElement eight() {
+        return driver.findElement(By.id("eight"));
+    }
+
+    public WebElement nine() {
+        return driver.findElement(By.id("nine"));
+    }
+
+    public WebElement zero() {
+        return driver.findElement(By.id("zero"));
+    }
+
+    public WebElement add() {
+        return driver.findElement(By.id("add"));
+    }
+
+    public WebElement subtract() {
+        return driver.findElement(By.id("subtract"));
+    }
+
+    public WebElement multiply() {
+        return driver.findElement(By.id("multiply"));
+    }
+
+    public WebElement divide() {
+        return driver.findElement(By.id("divide"));
+    }
+
+    public WebElement equals() {
+        return driver.findElement(By.id("equals"));
+    }
+
+    public WebElement clear() {
+        return driver.findElement(By.id("clear"));
+    }
 }
--- src/test/java/com/tests/santanderseleniumcalculadora/CalculatorTest.java
+++ src/test/java/com/tests/santanderseleniumcalculadora/CalculatorTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CalculadoraTest extends Locators {
+class CalculatorTest {
+    private static WebDriver driver;
+    private final CalculatorPage page = new CalculatorPage(driver);
 
     @BeforeAll
     static void setup() {
@@ -36,40 +39,171 @@ class CalculadoraTest extends Locators {
 
     @Test
     void PageTitle() {
-        assertEquals("Calculadora", $tagName("h1").getText());
+        assertEquals("Calculadora", page.title().getText());
     }
 
     @Test
     void PageDescription() {
         String expected = "Valide as operações básicas e utilize no mínimo 3 locators diferentes.";
-        assertEquals(expected, $x("//*[@id='display']").getText());
+        assertEquals(expected, page.description().getText());
     }
 
     @Test
     void addNumbersToDisplay() {
-        $id("one").click();
-        $id("two").click();
-        $id("three").click();
-        $id("four").click();
-        $id("five").click();
-        $id("six").click();
-        $id("seven").click();
-        $id("eight").click();
-        $id("nine").click();
-        $id("zero").click();
+        page.seven().click();
+        page.eight().click();
+        page.nine().click();
+        page.four().click();
+        page.five().click();
+        page.six().click();
+        page.one().click();
+        page.two().click();
+        page.three().click();
+        page.zero().click();
 
-        assertEquals("1234567890", $tagName("input").getAttribute("value"));
+        assertEquals("7894561230", page.display().value());
     }
 
     @Test
     void justOneOperatorSameTime() {
-        $id("one").click();
+        page.seven().click();
+        page.add().click();
+        page.multiply().click();
+        page.eight().click();
 
-        $id("add").click();
-        $id("sub").click();
-        $id("mul").click();
-        $id("div").click();
+        assertEquals("7*8", page.display().value());
 
-        assertEquals("1234567890", $tagName("input").getAttribute("value"));
+        page.equals().click();
+
+        assertEquals("56", page.display().value());
+    }
+
+    @Test
+    void buttonsText() {
+        assertEquals("1", page.one().getText());
+        assertEquals("2", page.two().getText());
+        assertEquals("3", page.three().getText());
+        assertEquals("4", page.four().getText());
+        assertEquals("5", page.five().getText());
+        assertEquals("6", page.six().getText());
+        assertEquals("7", page.seven().getText());
+        assertEquals("8", page.eight().getText());
+        assertEquals("9", page.nine().getText());
+        assertEquals("0", page.zero().getText());
+        assertEquals("+", page.add().getText());
+        assertEquals("-", page.subtract().getText());
+        assertEquals("*", page.multiply().getText());
+        assertEquals("/", page.divide().getText());
+        assertEquals("=", page.equals().getText());
+        assertEquals("C", page.clear().getText());
+    }
+
+    @Test
+    void addNumbers() {
+        page.three().click();
+        page.add().click();
+        page.five().click();
+
+        assertEquals("3+5", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("8", page.display().value());
+    }
+
+    @Test
+    void subtractNumbers() {
+        page.five().click();
+        page.subtract().click();
+        page.three().click();
+
+        assertEquals("5-3", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("2", page.display().value());
+    }
+
+    @Test
+    void multiplyNumbers() {
+        page.five().click();
+        page.multiply().click();
+        page.three().click();
+
+        assertEquals("5*3", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("15", page.display().value());
+    }
+
+    @Test
+    void divideNumbers() {
+        page.five().click();
+        page.divide().click();
+        page.three().click();
+
+        assertEquals("5/3", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("1.6666666666666667", page.display().value());
+    }
+
+    @Test
+    void divideByZero() {
+        page.five().click();
+        page.divide().click();
+        page.zero().click();
+
+        assertEquals("5/0", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("Infinity", page.display().value());
+    }
+
+    @Test
+    void clearDisplay() {
+        page.five().click();
+        page.divide().click();
+        page.zero().click();
+
+        assertEquals("5/0", page.display().value());
+
+        page.clear().click();
+        
+        assertEquals("", page.display().value());
+    }
+
+    @Test
+    void multipleOperations() {
+        page.five().click();
+        page.add().click();
+        page.three().click();
+        page.multiply().click();
+        page.five().click();
+        page.subtract().click();
+        page.four().click();
+        page.divide().click();
+        page.two().click();
+
+        assertEquals("5+3*5-4/2", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("18", page.display().value());
+    }
+
+    @Test
+    void notSecondNumber() {
+        page.five().click();
+        page.multiply().click();
+
+        assertEquals("5*", page.display().value());
+
+        page.equals().click();
+
+        assertEquals("5*", page.display().value());
     }
 }
